d6c1ec210c2bf51599d0264ae42a9fc92aa9f98e
Sakshi Gupta <sakshigupta8170@gmail.com>
2026-04-21 22:09:49 +0530

    Create client.py

---
+++ bot/client.py
@@ -0,0 +1,9 @@
+from binance.client import Client
+
+class BinanceClient:
+    def __init__(self, api_key, api_secret):
+        self.client = Client(api_key, api_secret)
+        self.client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
+
+    def get_client(self):
+        return self.client
